8cfbacec1b94b262460b0b726ab2f15837499166
unknown <goldenfinix@outlook.com>
2024-06-25 11:59:19 -0400

    Add processDate module in utils

---
+++ src/utils/processDate.tsx
@@ -0,0 +1,12 @@
+export const formatDate = (date: Date): string => {
+  const options: Intl.DateTimeFormatOptions = {
+    month: "short",
+    day: "numeric",
+  };
+  return date.toLocaleDateString("en-US", options);
+};
+
+export const toLocalDate = (dateString: string): Date => {
+  const [year, month, day] = dateString.split("-").map(Number);
+  return new Date(year, month - 1, day);
+};
