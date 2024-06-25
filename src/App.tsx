91f61c92b23e0a36d7b7f810cd8f3435657be48d
unknown <goldenfinix@outlook.com>
2024-06-25 10:40:34 -0400

    Configured routing in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { Direction } from "./type";
+import { HeaderComponent } from "./components/header/header.component";
+import HomePage from "./pages/home/home.page";
+import TourDetailPage from "./pages/tour.detail/tour.detail";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
+  const [direction, setDirection] = useState<Direction>(1); // 1 for forward, -1 for backward
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderComponent />
+      <AnimatePresence mode="wait" custom={direction}>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<HomePage setDirection={setDirection} />} />
+          <Route
+            path="/page1"
+            element={
+              <TourDetailPage
+                direction={direction}
+                setDirection={setDirection}
+              />
+            }
+          />
+          <Route
+            path="/page2"
+            element={
+              <TourDetailPage
+                direction={direction}
+                setDirection={setDirection}
+              />
+            }
+          />
+          {/* Add other routes similarly */}
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>
 );
 
