e331cd0c0b8cb12194c7e20326b24800b1d673d3
unknown <goldenfinix@outlook.com>
2024-06-25 04:50:21 -0400

    Configure TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,mjs,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#279a45",
+        secondary: "#ff991f",
+        darkgreen: "#055424",
+        darkyellow: "#c56600",
+      },
+    },
+  },
+  plugins: [],
+};
