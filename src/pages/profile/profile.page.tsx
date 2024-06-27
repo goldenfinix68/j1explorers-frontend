12fd7f822c926caa4a904e688fd951014a4281f1
goldenfinix68 <goldenfinix@outlook.com>
2024-06-27 01:12:26 -0400

    Add the raw profile page

---
+++ src/pages/profile/profile.page.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+export const ProfilePage: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <div>Your profile</div>
+        <div>
+          <div>Name:</div>
+          <div>John Sanderson</div>
+        </div>
+        <div>
+          <div>Gender:</div>
+          <div>Male</div>
+        </div>
+        <div>
+          <div>Birthdate:</div>
+          <div>2 February 20xx</div>
+        </div>
+        <div>
+          <div>Email:</div>
+          <div>jsanderson@j1explorers.com</div>
+        </div>
+        <div>
+          <div>Phone:</div>
+          <div>+1 307-757-6430</div>
+        </div>
+        <div>
+          <div>WhatsApp:</div>
+          <div>+1 307-757-6430</div>
+        </div>
+        <div>
+          <div>Employer:</div>
+          <div>Fun Lodge</div>
+        </div>
+        <div>
+          <div>Passport country:</div>
+          <div>United States</div>
+        </div>
+        <div>
+          <div>Birth city:</div>
+          <div>Manhattan</div>
+        </div>
+        <div>
+          <div>Passport #:</div>
+          <div>xxx xxx xxxx</div>
+        </div>
+        <div>
+          <div>Expiration date:</div>
+          <div>2 February 20xx</div>
+        </div>
+      </div>
+      <div>
+        <div>Pack members</div>
+        <div>
+          <div>Member 1:</div>
+          <div>John Sanderson</div>
+        </div>
+        <div>
+          <div>Member 2:</div>
+          <div>John Sanderson</div>
+        </div>
+        <div>
+          <div>Member 3:</div>
+          <div>John Sanderson</div>
+        </div>
+        <div>
+          <div>Member 4:</div>
+          <div>John Sanderson</div>
+        </div>
+      </div>
+    </div>
+  );
+};
