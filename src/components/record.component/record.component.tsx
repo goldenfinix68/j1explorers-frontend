9df5e918bdf77840b70f545b70f7d7c52655e63f
goldenfinix68 <goldenfinix@outlook.com>
2024-06-27 03:54:27 -0400

    Add default values to RecordComponentProps

--- src/components/record.component/record.component.tsx
+++ src/components/record.component/record.component.tsx
@@ -2,23 +2,24 @@ import React from "react";
 interface RecordComponentProps {
   title: string;
   description: any;
-  title_style: string;
-  description_style: string;
-  gap: string;
-  className: string;
+  title_style?: string;
+  description_style?: string;
+  gap?: string;
+  className?: string;
 }
 export const RecordComponent: React.FC<RecordComponentProps> = ({
   title,
   description,
-  title_style,
-  description_style,
-  gap,
-  className,
+  title_style = "",
+  description_style = "",
+  gap = ":&nbsp;",
+  className = "flex",
 }) => {
   return (
     <div className={className}>
-      <div className={`${title_style}`}>{`${title}${gap}`}</div>
-      <div className={`${description_style}`}>{description}</div>
+      <div className={title_style}>{`${title}`}</div>
+      <span className={title_style} dangerouslySetInnerHTML={{ __html: gap }} />
+      <div className={description_style}>{description}</div>
     </div>
   );
 };
