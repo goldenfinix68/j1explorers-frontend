bc8b66b366b650e7067154624ea464e9b2fe8256
goldenfinix68 <goldenfinix@outlook.com>
2024-06-27 03:58:53 -0400

    Format profile page

--- src/pages/profile/profile.page.tsx
+++ src/pages/profile/profile.page.tsx
@@ -1,73 +1,52 @@
 import React from "react";
+import RecordComponent from "../../components/record.component";
+import { RecordDetail } from "../../type";
 
 export const ProfilePage: React.FC = () => {
+  const profile: RecordDetail[] = [
+    { title: "Name", description: "John Sanderson" },
+    { title: "Gender", description: "Male" },
+    { title: "Birthdate", description: "2 February 20xx" },
+    { title: "Email", description: "jsanderson@j1explorers.com" },
+    { title: "Phone", description: "+1 307-757-6430" },
+    { title: "WhatsApp", description: "+1 307-757-6430" },
+    { title: "Employer", description: "Fun Lodge" },
+    { title: "Passport country", description: "United States" },
+    { title: "Birth city", description: "Manhattan" },
+    { title: "Passport #", description: "xxx xxx xxxx" },
+    { title: "Expiration date", description: "2 February 20xx" },
+  ];
+  const packMembers: RecordDetail[] = [
+    { title: "Member 1", description: "John Sanderson" },
+    { title: "Member 2", description: "John Sanderson" },
+    { title: "Member 3", description: "John Sanderson" },
+    { title: "Member 4", description: "John Sanderson" },
+  ];
   return (
-    <div>
+    <div className="text-[19.33px] text-center font-bold mx-3 pl-8">
       <div>
-        <div>Your profile</div>
-        <div>
-          <div>Name:</div>
-          <div>John Sanderson</div>
-        </div>
-        <div>
-          <div>Gender:</div>
-          <div>Male</div>
-        </div>
-        <div>
-          <div>Birthdate:</div>
-          <div>2 February 20xx</div>
-        </div>
-        <div>
-          <div>Email:</div>
-          <div>jsanderson@j1explorers.com</div>
-        </div>
-        <div>
-          <div>Phone:</div>
-          <div>+1 307-757-6430</div>
-        </div>
-        <div>
-          <div>WhatsApp:</div>
-          <div>+1 307-757-6430</div>
-        </div>
-        <div>
-          <div>Employer:</div>
-          <div>Fun Lodge</div>
-        </div>
-        <div>
-          <div>Passport country:</div>
-          <div>United States</div>
-        </div>
-        <div>
-          <div>Birth city:</div>
-          <div>Manhattan</div>
-        </div>
-        <div>
-          <div>Passport #:</div>
-          <div>xxx xxx xxxx</div>
-        </div>
-        <div>
-          <div>Expiration date:</div>
-          <div>2 February 20xx</div>
-        </div>
+        <div className="text-[29px] text-secondary">Your profile</div>
+        {profile.map(({ title, description }) => (
+          <RecordComponent
+            key={title}
+            title={title}
+            description={description}
+            title_style="text-secondary"
+            description_style="text-darkyellow"
+          />
+        ))}
       </div>
       <div>
-        <div>Pack members</div>
-        <div>
-          <div>Member 1:</div>
-          <div>John Sanderson</div>
-        </div>
-        <div>
-          <div>Member 2:</div>
-          <div>John Sanderson</div>
-        </div>
-        <div>
-          <div>Member 3:</div>
-          <div>John Sanderson</div>
-        </div>
-        <div>
-          <div>Member 4:</div>
-          <div>John Sanderson</div>
-        </div>
+        <div className="text-[29px] text-secondary">Pack members</div>
+        {packMembers.map(({ title, description }) => (
+          <RecordComponent
+            key={title}
+            title={title}
+            description={description}
+            title_style="text-secondary"
+            description_style="text-darkyellow"
+          />
+        ))}
       </div>
     </div>
   );
