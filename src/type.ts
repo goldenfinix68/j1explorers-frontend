5aa8420863adffb5d9d346809b797d6bb55ed5bc
goldenfinix68 <goldenfinix@outlook.com>
2024-06-27 03:41:49 -0400

    Add RecordDetail interface

--- src/type.ts
+++ src/type.ts
@@ -1,1 +1,10 @@
 export type Direction = 1 | -1;
+
+export interface RecordDetail {
+  title: string;
+  description: any;
+  title_style?: string;
+  description_style?: string;
+  gap?: string;
+  className?: string;
+}
