c5783cdd438982449be228f53c8bd0d714fa5e18
goldenfinix68 <goldenfinix@outlook.com>
2024-06-28 18:56:55 -0400

    Add CITIES & CONTACTS const data

---
+++ src/consts/index.ts
@@ -0,0 +1,12 @@
+export const CITIES: Record<string, string> = {
+  la: "LA",
+  vegas: "Vegas",
+  nyc: "NYC",
+};
+
+export const CONTACTS: Record<string, string> = {
+  hotel: "hotel",
+  "fly-away": "FlyAway",
+  "hop-on-off": "hop-on/off",
+  coordinator: "J1E coordinators",
+};
