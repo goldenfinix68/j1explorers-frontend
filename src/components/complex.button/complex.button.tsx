8a30b41ca647260c6134901af7b8530f44561945
goldenfinix68 <goldenfinix@outlook.com>
2024-06-28 16:17:58 -0400

    Add complex button component

---
+++ src/components/complex.button/complex.button.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface ComplexButtonProps {
+  left_title: string[];
+  left_className: string;
+  right_title: string;
+  right_className: string;
+  className?: string;
+  onClick: () => void;
+}
+export const ComplexButtonComponent: React.FC<ComplexButtonProps> = ({
+  left_title,
+  left_className,
+  right_className,
+  right_title,
+  className = "flex w-full leading-none text-white font-light h-[66px] text-[18.25px] mb-1",
+  onClick,
+}) => {
+  return (
+    <div className={className} onClick={() => onClick()}>
+      <div
+        className={`${left_className} flex justify-between items-center w-[106px] pl-2 border-[3px] border-r-0 rounded-l-xl`}
+      >
+        <div className="flex flex-col">
+          {left_title.map((val) => (
+            <span>{val}</span>
+          ))}
+        </div>
+        <span className="text-[27.375px] leading-none">|</span>
+      </div>
+      <div
+        className={`${right_className} flex justify-between items-center grow pl-[11px] pr-2 border-[3px] border-l-0 rounded-r-xl`}
+      >
+        <span>{right_title}</span>
+        <img src={`${process.env.PUBLIC_URL}/assets/images/right.png`} />
+      </div>
+    </div>
+  );
+};
---
+++ src/components/complex.button/index.ts
@@ -0,0 +1,3 @@
+import { ComplexButtonComponent } from "./complex.button";
+
+export default ComplexButtonComponent;
