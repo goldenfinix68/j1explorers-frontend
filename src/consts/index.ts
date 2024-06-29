77e2ad499b8adf1871ffc18c7ec9cc2e860fdf91
goldenfinix68 <goldenfinix@outlook.com>
2024-06-29 03:54:32 -0400

    Refactor consts data module

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,12 +1,2 @@
-export const CITIES: Record<string, string> = {
-  la: "LA",
-  vegas: "Vegas",
-  nyc: "NYC",
-};
-
-export const CONTACTS: Record<string, string> = {
-  hotel: "hotel",
-  "fly-away": "FlyAway",
-  "hop-on-off": "hop-on/off",
-  coordinator: "J1E coordinators",
-};
+export * from "./styles";
+export * from "./data";
