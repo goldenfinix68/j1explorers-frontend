999be53c6e707432b7ec116da6ebb115afd73121
goldenfinix68 <goldenfinix@outlook.com>
2024-06-29 10:29:20 -0400

    Update color consts in consts style

--- src/consts/styles.ts
+++ src/consts/styles.ts
@@ -1,5 +1,23 @@
-export const COLORS: Record<string, string> = {
+export const BUTTON_COLORS: Record<string, string> = {
   la: "bg-primary border-darkgreen",
   vegas: "bg-darkyellow border-secondary",
   nyc: "bg-secondary border-darkyellow",
+};
+
+export const DIVIDER_COLORS: Record<string, string> = {
+  la: "border-primary",
+  vegas: "border-darkyellow",
+  nyc: "border-secondary",
+};
+
+export const TITLE_COLORS: Record<string, string> = {
+  la: "text-primary",
+  vegas: "text-darkyellow",
+  nyc: "text-secondary",
+};
+
+export const CONTENT_COLORS: Record<string, string> = {
+  la: "text-darkgreen",
+  vegas: "text-secondary",
+  nyc: "text-darkyellow",
 };
