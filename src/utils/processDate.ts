50b883b608da9b59476f141dd46bdea75a55eb85
goldenfinix68 <goldenfinix@outlook.com>
2024-06-30 12:47:55 -0400

    Refactor processDate.tsx to ts

--- src/utils/processDate.ts
+++ src/utils/processDate.ts
@@ -10,3 +10,7 @@ export const toLocalDate = (dateString: string): Date => {
   const [year, month, day] = dateString.split("-").map(Number);
   return new Date(year, month - 1, day);
 };
+
+export const removeSuffix = (dateStr: string): string => {
+  return dateStr.replace(/(\d+)(st|nd|rd|th)/, "$1");
+};
