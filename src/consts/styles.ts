c037646fa0408fa91a69d72666a9b506804bc47c
goldenfinix68 <goldenfinix@outlook.com>
2024-06-30 15:14:45 -0400

    Update key type from string to Location

--- src/consts/styles.ts
+++ src/consts/styles.ts
@@ -1,22 +1,24 @@
-export const BUTTON_COLORS: Record<string, string> = {
+import { Location } from "../type";
+
+export const BUTTON_COLORS: Record<Location, string> = {
   la: "bg-primary border-darkgreen",
   vegas: "bg-darkyellow border-secondary",
   nyc: "bg-secondary border-darkyellow",
 };
 
-export const DIVIDER_COLORS: Record<string, string> = {
+export const DIVIDER_COLORS: Record<Location, string> = {
   la: "border-primary",
   vegas: "border-darkyellow",
   nyc: "border-secondary",
 };
 
-export const TITLE_COLORS: Record<string, string> = {
+export const TITLE_COLORS: Record<Location, string> = {
   la: "text-primary",
   vegas: "text-darkyellow",
   nyc: "text-secondary",
 };
 
-export const CONTENT_COLORS: Record<string, string> = {
+export const CONTENT_COLORS: Record<Location, string> = {
   la: "text-darkgreen",
   vegas: "text-secondary",
   nyc: "text-darkyellow",
