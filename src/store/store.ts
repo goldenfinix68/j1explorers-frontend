70694c998b82fb43745190c8c1760f9fb0306e32
goldenfinix68 <goldenfinix@outlook.com>
2024-07-05 05:34:57 -0400

    Add user redux query service in store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,16 +3,22 @@ import userReducer from "./user";
 import tourReducer from "./tour";
 import { categoryApi } from "../service/categoryService";
 import { faqApi } from "../service/faqService";
+import { userApi } from "../service/userService";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     tour: tourReducer,
+    [userApi.reducerPath]: userApi.reducer,
     [categoryApi.reducerPath]: categoryApi.reducer,
     [faqApi.reducerPath]: faqApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(categoryApi.middleware, faqApi.middleware),
+    getDefaultMiddleware().concat(
+      userApi.middleware,
+      categoryApi.middleware,
+      faqApi.middleware
+    ),
 });
 
 export default store;
