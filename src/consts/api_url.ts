7bf44f752d7aeab7011d78ea2f2ad69034eb7205
goldenfinix68 <goldenfinix@outlook.com>
2024-07-05 05:36:34 -0400

    Add user api url

--- src/consts/api_url.ts
+++ src/consts/api_url.ts
@@ -2,6 +2,13 @@ export const BASE_URL = "http://localhost:3000/api";
 
 export const api_url = {
   root: BASE_URL,
+  user: {
+    root: `${BASE_URL}/user`,
+    me: {
+      root: `${BASE_URL}/user/me`,
+      update: `${BASE_URL}/user/me/update`,
+    },
+  },
   category: {
     root: `${BASE_URL}/category`,
     fetchCategories: `${BASE_URL}/category/fetchCategories`,
