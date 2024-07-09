31a17bebd20310501c4a9f0a28567fc24ac22bc6
goldenfinix68 <goldenfinix@outlook.com>
2024-07-09 04:35:57 -0400

    Add settings nav button

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -2,9 +2,22 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { NavButtonComponent } from "../../components/nav.button/nav.button";
 import HeaderComponent from "../../components/header";
+import { useDispatch } from "react-redux";
+import { Direction } from "../../type";
+import { setDirection } from "../../store/direction/direction.slice";
 
 export const HomePage: React.FC = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleDirection = (direction: Direction) => {
+    dispatch(setDirection(direction));
+  };
+
+  const handleNavigation = (link: string) => {
+    handleDirection(1);
+    navigate(link);
+  };
 
   const menu = [
     { path: "/tour-detail", title: "Your Tour Details" },
@@ -15,6 +28,7 @@ export const HomePage: React.FC = () => {
     { path: "/faq", title: "FAQs" },
     { path: "/terms", title: "Terms & Conditions" },
     { path: "/j1e", title: "J1E Website/Flyer" },
+    { path: "/setting", title: "Account Settings" },
   ];
 
   return (
@@ -29,7 +43,7 @@ export const HomePage: React.FC = () => {
         {menu.map(({ path, title }) => (
           <NavButtonComponent
             key={title}
-            onClick={() => navigate(path)}
+            onClick={() => handleNavigation(path)}
             title={title}
             className="bg-primary border-darkgreen text-[30px] mb-1 py-[11px]"
           />
