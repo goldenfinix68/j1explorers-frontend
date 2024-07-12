36586c9ec822467d723bb2380d03cf468a0a897d
goldenfinix68 <goldenfinix@outlook.com>
2024-07-12 05:17:20 -0400

    Add FingerprintLoad Component

---
+++ src/components/fingerprint.load/fingerprint.load.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import FingerprintJS from "@fingerprintjs/fingerprintjs";
+
+interface FingerprintLoadProps {
+  setFingerprint: (fingerprint: string) => void;
+  className?: string;
+}
+
+export const FingerprintLoad: React.FC<FingerprintLoadProps> = ({
+  setFingerprint,
+  className,
+}) => {
+  useEffect(() => {
+    const loadFingerprint = async () => {
+      const fp = await FingerprintJS.load();
+      const result = await fp.get();
+      setFingerprint(result.visitorId);
+    };
+
+    loadFingerprint();
+  }, []);
+
+  return (
+    <img
+      src={`${process.env.PUBLIC_URL}/assets/images/fingerprint.png`}
+      className={className}
+    />
+  );
+};
+
+export default FingerprintLoad;
