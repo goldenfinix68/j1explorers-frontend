e02feb6079779e69204aaa825f54cf7c620f7a7c
goldenfinix68 <goldenfinix@outlook.com>
2024-07-20 11:52:40 -0400

    Add notify functions in utils

---
+++ src/utils/notify.ts
@@ -0,0 +1,57 @@
+import { toast, Bounce } from "react-toastify";
+
+export const notifySuccess = (msg: string) => {
+  toast.success(msg, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    transition: Bounce,
+  });
+};
+
+export const notifyWarning = (msg: string) => {
+  toast.warn(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    transition: Bounce,
+  });
+};
+
+export const notifyError = (msg: string) => {
+  toast.error(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    transition: Bounce,
+  });
+};
+
+export const notifyInfo = (msg: string) => {
+  toast.info("🦄 Wow so easy!", {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    transition: Bounce,
+  });
+};
