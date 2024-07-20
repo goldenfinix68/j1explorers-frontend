56a3a51da6e056d7bf09f4aac363ebf7b516c900
goldenfinix68 <goldenfinix@outlook.com>
2024-07-20 06:32:48 -0400

    Remove unnecessary statements

--- src/pages/logout/logout.page.tsx
+++ src/pages/logout/logout.page.tsx
@@ -1,10 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Navigate, useNavigate } from "react-router-dom";
-import { NavButtonComponent } from "../../components/nav.button/nav.button";
-import { useDispatch } from "react-redux";
-import { Credentials, Direction } from "../../type";
-import { setDirection } from "../../store/direction/direction.slice";
-import { useLoginMutation } from "../../service/userService";
 import { useAuth } from "../../containers/auth.provider/auth.provider";
 
 export const LogoutPage: React.FC = () => {
