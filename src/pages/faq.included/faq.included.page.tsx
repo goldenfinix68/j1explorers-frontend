09be0310a6c6b325990dcc115efba51551ad67ed
goldenfinix68 <goldenfinix@outlook.com>
2024-07-23 11:50:06 -0400

    Update image src link in fag pages

--- src/pages/faq.included/faq.included.page.tsx
+++ src/pages/faq.included/faq.included.page.tsx
@@ -20,7 +20,7 @@ export const FAQIncludedPage: React.FC = () => {
     <>
       <HeaderComponent>
         <Link to="/faq/main" onClick={() => handleDirection(-1)}>
-          <img src={`${process.env.PUBLIC_URL}/assets/images/left_green.png`} />
+          <img src="/assets/images/left_green.png" />
         </Link>
         <div className="flex flex-col text-darkgreen text-[29px] text-center font-bold leading-[24.16px]">
           <span>FAQs</span>
