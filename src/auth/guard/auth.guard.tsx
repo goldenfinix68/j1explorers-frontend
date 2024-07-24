906aa3767991eab4ebb0bc9dbed94f778ec126bb
Golden Phoenix <goldenfinix@outlook.com>
2024-07-24 07:12:45 -0400

    Add splash screen in auth guard

--- src/auth/guard/auth.guard.tsx
+++ src/auth/guard/auth.guard.tsx
@@ -1,13 +1,14 @@
 import React, { ReactNode } from "react";
 import { Route, Navigate } from "react-router-dom";
 import { useAuth } from "../hooks";
+import { SplashScreen } from "../../components/loading-screen";
 
 export interface AuthGuardProps {
   children: ReactNode;
 }
 
 export const AuthGuard: React.FC<AuthGuardProps> = ({ children }) => {
-  const { user } = useAuth();
+  const { loading } = useAuth();
 
-  return user ? <div>{children}</div> : <Navigate to="/login" />;
+  return loading ? <SplashScreen /> : <div>{children}</div>;
 };
