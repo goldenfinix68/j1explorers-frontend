bff8abed08d85c0dcdb11065e563671149e2ed6a
Golden Phoenix <goldenfinix@outlook.com>
2024-08-03 15:10:13 -0400

    Remove unnecessary commands in fingerprint load modal

--- src/components/fingerprint.load/fingerprint.load.tsx
+++ src/components/fingerprint.load/fingerprint.load.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import FingerprintJS from "@fingerprintjs/fingerprintjs";
 
 interface FingerprintLoadProps {
